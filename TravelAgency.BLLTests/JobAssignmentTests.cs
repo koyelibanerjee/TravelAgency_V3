71291b98f009bf965aa7ca7e102afa56fd27f0c3
YXP <954222619@qq.com>
2018-03-22 11:31:19 +0800

    修正10s分配间隔判断错误

---
+++ TravelAgency.BLLTests/JobAssignmentTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TravelAgency.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAgency.BLL.Tests
+{
+    [TestClass()]
+    public class JobAssignmentTests
+    {
+        [TestMethod()]
+        public void AssignmentJobTest()
+        {
+            var now = DateTime.Now;
+            var later = DateTime.Now.AddHours(1);
+             Assert.IsTrue((later - now).Duration().TotalSeconds == 3600.0);
+        }
+    }
+}
