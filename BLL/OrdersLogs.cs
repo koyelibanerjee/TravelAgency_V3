abae42bfdf33932993b6fdec1ad8336d69173168
YXP <954222619@qq.com>
2018-05-13 21:17:10 +0800

    添加OrderLogs三层文件

---
+++ BLL/OrdersLogs.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using TravelAgency.Model;
+namespace TravelAgency.BLL
+{
+	/// <summary>
+	/// OrdersLogs
+	/// </summary>
+	public partial class OrdersLogs
+	{
+		private readonly TravelAgency.DAL.OrdersLogs dal=new TravelAgency.DAL.OrdersLogs();
+		public OrdersLogs()
+		{}
+		#region  BasicMethod
+
+		/// <summary>
+		/// 得到最大ID
+		/// </summary>
+		public int GetMaxId()
+		{
+			return dal.GetMaxId();
+		}
+
+		/// <summary>
+		/// 是否存在该记录
+		/// </summary>
+		public bool Exists(int id)
+		{
+			return dal.Exists(id);
+		}
+
+		/// <summary>
+		/// 增加一条数据
+		/// </summary>
+		public int  Add(TravelAgency.Model.OrdersLogs model)
+		{
+			return dal.Add(model);
+		}
+
+		/// <summary>
+		/// 更新一条数据
+		/// </summary>
+		public bool Update(TravelAgency.Model.OrdersLogs model)
+		{
+			return dal.Update(model);
+		}
+
+		/// <summary>
+		/// 删除一条数据
+		/// </summary>
+		public bool Delete(int id)
+		{
+			
+			return dal.Delete(id);
+		}
+		/// <summary>
+		/// 得到一个对象实体
+		/// </summary>
+		public TravelAgency.Model.OrdersLogs GetModel(int id)
+		{
+			return dal.GetModel(id);
+		}
+
+
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public DataSet GetList(string strWhere)
+		{
+			return dal.GetList(strWhere);
+		}
+		/// <summary>
+		/// 获得前几行数据
+		/// </summary>
+		public DataSet GetList(int Top,string strWhere,string filedOrder)
+		{
+			return dal.GetList(Top,strWhere,filedOrder);
+		}
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public List<TravelAgency.Model.OrdersLogs> GetModelList(string strWhere)
+		{
+			DataSet ds = dal.GetList(strWhere);
+			return DataTableToList(ds.Tables[0]);
+		}
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public List<TravelAgency.Model.OrdersLogs> DataTableToList(DataTable dt)
+		{
+			List<TravelAgency.Model.OrdersLogs> modelList = new List<TravelAgency.Model.OrdersLogs>();
+			int rowsCount = dt.Rows.Count;
+			if (rowsCount > 0)
+			{
+				TravelAgency.Model.OrdersLogs model;
+				for (int n = 0; n < rowsCount; n++)
+				{
+					model = dal.DataRowToModel(dt.Rows[n]);
+					if (model != null)
+					{
+						modelList.Add(model);
+					}
+				}
+			}
+			return modelList;
+		}
+
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public DataSet GetAllList()
+		{
+			return GetList("");
+		}
+
+		/// <summary>
+		/// 分页获取数据列表
+		/// </summary>
+		public int GetRecordCount(string strWhere)
+		{
+			return dal.GetRecordCount(strWhere);
+		}
+		/// <summary>
+		/// 分页获取数据列表
+		/// </summary>
+		public DataSet GetListByPage(string strWhere, string orderby, int startIndex, int endIndex)
+		{
+			return dal.GetListByPage( strWhere,  orderby,  startIndex,  endIndex);
+		}
+		/// <summary>
+		/// 分页获取数据列表
+		/// </summary>
+		//public DataSet GetList(int PageSize,int PageIndex,string strWhere)
+		//{
+			//return dal.GetList(PageSize,PageIndex,strWhere);
+		//}
+
+		#endregion  BasicMethod
+		#region  ExtensionMethod
+
+		#endregion  ExtensionMethod
+	}
+}
+
---
+++ Model/OrdersLogs.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace TravelAgency.Model
+{
+	/// <summary>
+	/// OrdersLogs:实体类(属性说明自动提取数据库字段的描述信息)
+	/// </summary>
+	[Serializable]
+	public partial class OrdersLogs
+	{
+		public OrdersLogs()
+		{}
+		#region Model
+		private int _id;
+		private int? _acttype;
+		private string _username;
+		private int? _ordersid;
+		private DateTime? _entrytime;
+		/// <summary>
+		/// 
+		/// </summary>
+		public int id
+		{
+			set{ _id=value;}
+			get{return _id;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public int? ActType
+		{
+			set{ _acttype=value;}
+			get{return _acttype;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public string UserName
+		{
+			set{ _username=value;}
+			get{return _username;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public int? OrdersId
+		{
+			set{ _ordersid=value;}
+			get{return _ordersid;}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public DateTime? EntryTime
+		{
+			set{ _entrytime=value;}
+			get{return _entrytime;}
+		}
+		#endregion Model
+
+	}
+}
+
