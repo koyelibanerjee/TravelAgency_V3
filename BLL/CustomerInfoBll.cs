5ec3e7874ef7f1f683d3acb1b13eb9546ea75c27
c437yuyang <954222619@qq.com>
2018-06-07 08:38:12 +0800

    客户添加departmentid筛选

--- BLL/CustomerInfoBll.cs
+++ BLL/CustomerInfoBll.cs
@@ -9,6 +9,9 @@ namespace TravelAgency.BLL
     /// </summary>
     public partial class CustomerInfo
     {
+
+
+        //其实这里可以直接用一个查询(连接)返回对应结构体(所有需要的封装起来)就行了
         public static List<KeyValuePair<Guid, string>> GetCustomerList()
         {
             return DAL.CustomerInfo.GetCustomerList();
--- DAL/CustomerInfoDal.cs
+++ DAL/CustomerInfoDal.cs
@@ -7,28 +7,28 @@ using System.Collections.Generic;
 
 namespace TravelAgency.DAL
 {
-	/// <summary>
-	/// 数据访问类:CustomerInfo
-	/// </summary>
-	public partial class CustomerInfo
-	{
-        public static List<KeyValuePair<Guid,string>> GetCustomerList()
+    /// <summary>
+    /// 数据访问类:CustomerInfo
+    /// </summary>
+    public partial class CustomerInfo
+    {
+        public static List<KeyValuePair<Guid, string>> GetCustomerList()
         {
-        string sql = "select distinct customername,customeid from CustomerInfo where " +
-                     "Operator is not null and LEN(Operator) > 0 and " +
-                     "CustomerName is not null and LEN(CustomerName)>0 and " +
-                     "DataType = '销售录入'";
+            string sql = "select distinct customername,customeid from CustomerInfo where " +
+                         "CustomerName is not null and LEN(CustomerName)>0 and " +
+                         "DataType = '销售录入' and" +
+                         " DepartmentId='a86ed375-76db-45df-a4e9-d0bb8815d49c'";
             DataSet ds = DbHelperSQL.Query(sql);
             List<KeyValuePair<Guid, string>> res = new List<KeyValuePair<Guid, string>>();
             if (ds.Tables[0].Rows.Count > 0)
             {
                 foreach (DataRow row in ds.Tables[0].Rows)
                 {
-                    if (row["customeid"] != null && row["customeid"].ToString() != "" && 
+                    if (row["customeid"] != null && row["customeid"].ToString() != "" &&
                         row["customername"] != null && row["customername"].ToString() != "")
                     {
                         res.Add(new KeyValuePair<Guid, string>
-                            (Guid.Parse(row["customeid"].ToString()), 
+                            (Guid.Parse(row["customeid"].ToString()),
                             row["customername"].ToString()));
                     }
                 }
