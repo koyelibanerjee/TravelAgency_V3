1fe71f6c5ed33c7c588ec26925a8603b3f01a52e
c437yuyang <954222619@qq.com>
2018-08-17 09:47:25 +0800

    增加两个工具类

---
+++ TravelAgency.CommonTests/WebClientTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TravelAgency.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAgency.Common.Tests
+{
+    [TestClass()]
+    public class WebClientTests
+    {
+        [TestMethod()]
+        public void GetDataTest()
+        {
+            string ret = new WebClient().GetHtml("http://www.httpbin.org/get");
+
+            var data = new WebClient().GetData("http://www.httpbin.org/get");
+            ret = System.Text.Encoding.Default.GetString(data);
+        }
+
+        [TestMethod()]
+        public void PostTest()
+        {
+            string data = "{'test string':1,'b':a}";
+            var ret = new WebClient().Post("http://www.httpbin.org/post", data);
+        }
+    }
+}
