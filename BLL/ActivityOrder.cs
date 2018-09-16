a707e67639d526b9918fe67e8d74064dc611d987
c437yuyang <954222619@qq.com>
2018-09-16 19:45:34 +0800

    添加ActivityOrder三层文件

---
+++ BLL/ActivityOrder.cs
@@ -0,0 +1,198 @@
+﻿using System; 
+using System.Text;
+using System.Collections.Generic; 
+using System.Data;
+using TravelAgency.Model;
+namespace TravelAgency.BLL  
+{
+	 	//ActivityOrder
+		public partial class ActivityOrder
+	{
+   		     
+		private readonly TravelAgency.DAL.ActivityOrder dal=new TravelAgency.DAL.ActivityOrder();
+		public ActivityOrder()
+		{}
+		
+		#region  Method
+		/// <summary>
+		/// 是否存在该记录
+		/// </summary>
+		public bool Exists(string ActivityOrderNo)
+		{
+			return dal.Exists(ActivityOrderNo);
+		}
+
+		/// <summary>
+		/// 增加一条数据
+		/// </summary>
+		    public void Add(TravelAgency.Model.ActivityOrder model)
+    		{
+            dal.Add(model);
+      		}
+
+		/// <summary>
+		/// 更新一条数据
+		/// </summary>
+		public bool Update(TravelAgency.Model.ActivityOrder model)
+		{
+			return dal.Update(model);
+		}
+
+		/// <summary>
+		/// 删除一条数据
+		/// </summary>
+		public bool Delete(string ActivityOrderNo)
+		{
+			
+			return dal.Delete(ActivityOrderNo);
+		}
+
+		/// <summary>
+		/// 得到一个对象实体
+		/// </summary>
+		public TravelAgency.Model.ActivityOrder GetModel(string ActivityOrderNo)
+		{
+			
+			return dal.GetModel(ActivityOrderNo);
+		}
+
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public DataSet GetList(string strWhere)
+		{
+			return dal.GetList(strWhere);
+		}
+		/// <summary>
+		/// 获得前几行数据
+		/// </summary>
+		public DataSet GetList(int Top,string strWhere,string filedOrder)
+		{
+			return dal.GetList(Top,strWhere,filedOrder);
+		}
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public List<TravelAgency.Model.ActivityOrder> GetModelList(string strWhere)
+		{
+			DataSet ds = dal.GetList(strWhere);
+			return DataTableToList(ds.Tables[0]);
+		}
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public List<TravelAgency.Model.ActivityOrder> DataTableToList(DataTable dt)
+		{
+			List<TravelAgency.Model.ActivityOrder> modelList = new List<TravelAgency.Model.ActivityOrder>();
+			int rowsCount = dt.Rows.Count;
+			if (rowsCount > 0)
+			{
+				TravelAgency.Model.ActivityOrder model;
+				for (int n = 0; n < rowsCount; n++)
+				{
+					model = dal.DataRowToModel(dt.Rows[n]);			
+          if(model!=null)
+            modelList.Add(model);
+				}
+			}
+			return modelList;
+		}
+
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public DataSet GetAllList()
+		{
+			return GetList("");
+		}
+		
+    /// <summary>
+		/// 获取记录总数
+		/// </summary>
+		public int GetRecordCount(string strWhere)
+		{
+			return dal.GetRecordCount(strWhere);
+		}
+		
+    /// <summary>
+    /// 分页获取数据
+    /// </summary>
+    public List<TravelAgency.Model.ActivityOrder> GetListByPage(string where, string orderby,int startIndex, int endIndex)
+    {
+        DataSet ds = dal.GetListByPage(where, orderby, startIndex, endIndex);
+        DataTable dt = ds.Tables[0];
+        return DataTableToList(dt);
+    }
+    
+    /// <summary>
+    /// 分页获取数据
+    /// </summary>
+    public List<TravelAgency.Model.ActivityOrder> GetListByPage(int pageIndex, int pageSize, string where, string orderby)
+    {
+        int startIndex = (pageIndex - 1) * pageSize + 1;
+        int endIndex = pageIndex * pageSize;
+        return GetListByPage(where, orderby, startIndex, endIndex);
+    }
+    
+
+		
+		
+				
+    /// <summary>
+    /// 分页获取数据
+    /// </summary>
+    public List<TravelAgency.Model.ActivityOrder> GetListByPageOrderByPK(int pageIndex, int pageSize, string where)
+    {
+        int startIndex = (pageIndex - 1) * pageSize + 1;
+        int endIndex = pageIndex * pageSize;
+        string orderby = "";
+        orderby = "ActivityOrderNo desc";          
+        return GetListByPage(where, orderby, startIndex, endIndex);
+    }
+		
+    /// <summary>
+    /// 新增list
+    /// </summary>		
+    public int AddList(List<TravelAgency.Model.ActivityOrder> list)
+    {
+        int res = 0;
+        foreach (var item in list)
+        {
+                    res += dal.Add(item) ? 0 : 1; //返回值是bool
+                }
+        return res;
+    }
+
+    /// <summary>
+    /// 更新list
+    /// </summary>
+    public int UpdateList(List<TravelAgency.Model.ActivityOrder> list)
+    {
+        int res = 0;
+        foreach (var item in list)
+        {
+            res += dal.Update(item) ? 1 : 0; //返回值是id
+        }
+        return res;
+    }
+    
+    /// <summary>
+    /// 删除list
+    /// </summary>    
+    public bool DeleteList(List<TravelAgency.Model.ActivityOrder> list)
+    {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < list.Count; ++i)
+        {
+                        sb.AppendFormat("'{0}',", list[i].ActivityOrderNo);
+                    }
+        string str_id_list = sb.ToString().TrimEnd(',');
+        return dal.DeleteList(str_id_list);
+    }
+				
+		
+		
+#endregion
+   
+	}
+}
---
+++ Model/ActivityOrder.cs
@@ -0,0 +1,287 @@
+﻿using System; 
+using System.Text;
+using System.Collections.Generic; 
+using System.Data;
+namespace TravelAgency.Model
+{
+	 	//ActivityOrder
+		[Serializable]
+	public partial class ActivityOrder
+	{
+        		                  
+    private string _activityorderno;
+        		                  
+    private string _productname;
+        		                  
+    private string _type_t;
+        		                  
+    private int? _number;
+        		                  
+    private int? _books_set;
+        		                  
+    private int? _books_sum;
+        		                  
+    private int? _balancebooks;
+        		                  
+    private decimal? _amount;
+        		                  
+    private DateTime? _entrytime;
+        		                  
+    private string _amountpayable;
+        		                  
+    private string _tailmoney;
+        		                  
+    private DateTime? _entrytimepay;
+        		                  
+    private string _amountpaid;
+        		                  
+    private string _customername;
+        		                  
+    private string _company;
+        		                  
+    private string _mobile;
+        		                  
+    private string _deposit;
+        		                  
+    private string _price_active;
+        		                  
+    private string _paystate;
+        		                  
+    private string _type_sale;
+        		                  
+    private string _activityname;
+        
+    /// <summary>
+		/// ActivityOrderNo
+        /// </summary>		
+		      		  
+  
+        public string ActivityOrderNo
+        {
+            get{ return _activityorderno; }
+            set{ _activityorderno = value; }
+        }    
+      
+		/// <summary>
+		/// 产品名称
+        /// </summary>		
+		      		  
+  
+        public string ProductName
+        {
+            get{ return _productname; }
+            set{ _productname = value; }
+        }    
+      
+		/// <summary>
+		/// 抢购类型
+        /// </summary>		
+		      		  
+  
+        public string Type_T
+        {
+            get{ return _type_t; }
+            set{ _type_t = value; }
+        }    
+      
+		/// <summary>
+		/// 购买套数
+        /// </summary>		
+		        		  
+  
+        public int? Number
+        {
+            get{ return _number; }
+            set{ _number = value; }
+        }    
+      
+		/// <summary>
+		/// 每套本数
+        /// </summary>		
+		        		  
+  
+        public int? Books_Set
+        {
+            get{ return _books_set; }
+            set{ _books_set = value; }
+        }    
+      
+		/// <summary>
+		/// 总购买本数
+        /// </summary>		
+		        		  
+  
+        public int? Books_Sum
+        {
+            get{ return _books_sum; }
+            set{ _books_sum = value; }
+        }    
+      
+		/// <summary>
+		/// 使用后剩余的本数
+        /// </summary>		
+		        		  
+  
+        public int? BalanceBooks
+        {
+            get{ return _balancebooks; }
+            set{ _balancebooks = value; }
+        }    
+      
+		/// <summary>
+		/// 总金额
+        /// </summary>		
+		        		  
+  
+        public decimal? Amount
+        {
+            get{ return _amount; }
+            set{ _amount = value; }
+        }    
+      
+		/// <summary>
+		/// 购买时间
+        /// </summary>		
+		        		  
+  
+        public DateTime? EntryTime
+        {
+            get{ return _entrytime; }
+            set{ _entrytime = value; }
+        }    
+      
+		/// <summary>
+		/// 应付定金
+        /// </summary>		
+		      		  
+  
+        public string AmountPayable
+        {
+            get{ return _amountpayable; }
+            set{ _amountpayable = value; }
+        }    
+      
+		/// <summary>
+		/// 尾款
+        /// </summary>		
+		      		  
+  
+        public string TailMoney
+        {
+            get{ return _tailmoney; }
+            set{ _tailmoney = value; }
+        }    
+      
+		/// <summary>
+		/// 支付时间
+        /// </summary>		
+		        		  
+  
+        public DateTime? EntryTimePay
+        {
+            get{ return _entrytimepay; }
+            set{ _entrytimepay = value; }
+        }    
+      
+		/// <summary>
+		/// 已付定金
+        /// </summary>		
+		      		  
+  
+        public string AmountPaid
+        {
+            get{ return _amountpaid; }
+            set{ _amountpaid = value; }
+        }    
+      
+		/// <summary>
+		/// 客户
+        /// </summary>		
+		      		  
+  
+        public string CustomerName
+        {
+            get{ return _customername; }
+            set{ _customername = value; }
+        }    
+      
+		/// <summary>
+		/// 公司
+        /// </summary>		
+		      		  
+  
+        public string Company
+        {
+            get{ return _company; }
+            set{ _company = value; }
+        }    
+      
+		/// <summary>
+		/// 手机
+        /// </summary>		
+		      		  
+  
+        public string Mobile
+        {
+            get{ return _mobile; }
+            set{ _mobile = value; }
+        }    
+      
+		/// <summary>
+		/// 产品要求定金
+        /// </summary>		
+		      		  
+  
+        public string Deposit
+        {
+            get{ return _deposit; }
+            set{ _deposit = value; }
+        }    
+      
+		/// <summary>
+		/// 活动单价
+        /// </summary>		
+		      		  
+  
+        public string Price_Active
+        {
+            get{ return _price_active; }
+            set{ _price_active = value; }
+        }    
+      
+		/// <summary>
+		/// 支付状态
+        /// </summary>		
+		      		  
+  
+        public string PayState
+        {
+            get{ return _paystate; }
+            set{ _paystate = value; }
+        }    
+      
+		/// <summary>
+		/// 活动类型
+        /// </summary>		
+		      		  
+  
+        public string Type_Sale
+        {
+            get{ return _type_sale; }
+            set{ _type_sale = value; }
+        }    
+      
+		/// <summary>
+		/// ActivityName
+        /// </summary>		
+		      		  
+  
+        public string ActivityName
+        {
+            get{ return _activityname; }
+            set{ _activityname = value; }
+        }    
+      
+		   
+	}
+}
